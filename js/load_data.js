7fa7f2a6f6f52b5e8717d82366d9768f062afd6c
Frank Ruiz <64100296+fruizotero@users.noreply.github.com>
2022-11-30 14:49:38 +0100

    Cambiando ruta de los datos

--- js/load_data.js
+++ js/load_data.js
@@ -3,7 +3,7 @@
 export const fetchData = async () => {
     try {
 
-        let res = await fetch("../data.json");
+        let res = await fetch("assets/data.json");
         let json = await res.json();
 
         if (!res.ok) throw { status: res.status, statusText: res.statusText }
